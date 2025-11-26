9c598d393f3003d5dba3af30b97917e71406c0d2
DomenGoli <domen.goli@gmail.com>
2025-11-26 17:42:38 +0100

    responsive designs

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,6 +1,6 @@
 function Title() {
     return (
-        <div className=" mb-5 mt-4">
+        <div className=" sm:mb-5 mt-4">
             <h1 className="text-5xl text-center font-bold text-shadow-fuchsia-900">Število dni do upokojitve</h1>
         </div>
     );
