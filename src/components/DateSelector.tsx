36e5694481eab77d6a84170cbd6a73b318cc8dc9
DomenGoli <domen.goli@gmail.com>
2025-11-27 17:01:24 +0100

    modal position fix

--- src/components/DateSelector.tsx
+++ src/components/DateSelector.tsx
@@ -20,8 +20,8 @@ function DateSelector({
     return (
         <div role="overlay" className="fixed items-center justify-center top-0 left-0 w-[100%] h-[100vh] z-1000 transition-all duration-[0.5s]">
             <div ref={ref}
-                style={{ top: viewPortalWidth > 500 ? position.y - 424 : position.y - 432, left: viewPortalWidth > 500 ? position.x-225 : position.x-130  }}
-                className="fixed flex justify-center w-[430px] h-[355px] top-96 rounded-2xl shadow-2xl bg-linear-to-tl from-[#753682] to-[#bf2e34] p-4 transition-all duration-2000 border-1 border-stone-700"
+                style={{ top: viewPortalWidth > 500 ? position.y - 424 : position.y - 432, left: viewPortalWidth > 500 ? position.x-221 : position.x-112  }}
+                className="fixed flex justify-center sm:w-[430px] w-[100%] h-[355px] top-96 rounded-2xl shadow-2xl bg-linear-to-tl from-[#753682] to-[#bf2e34] p-4 transition-all duration-2000 border-1 border-stone-700"
             >
                 <DayPicker
                     mode="single"
